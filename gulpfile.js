0dbaa74510f5f1d5117728b5d9a603ea6dc89550
Yiu Man Chi <ymcatar@gmail.com>
2016-09-17 11:21:25 +0800

    update: fix clean, add static file copying

--- gulpfile.js
+++ gulpfile.js
@@ -2,22 +2,22 @@ var gulp = require('gulp');
 var babel = require('gulp-babel');
 var clean = require('gulp-clean');
 
-gulp.task('babel', function () {
+gulp.task('clean', function () {
+  return gulp.src('dist/**/*.*')
+    .pipe(clean());
+});
+
+gulp.task('babel', ['clean'], function () {
   return gulp.src('src/**/*.js')
     .pipe(babel())
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('html', function () {
-  return gulp.src('src/**/*.html')
+gulp.task('static', ['clean'], function () {
+  return gulp.src(['src/**/*.**', '!src/**/*.js'])
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('clean', function () {
-  return gulp.src('dist/**/*.*', { read: false, force: true })
-    .pipe(clean());
-});
-
 gulp.task('default', function () {
-  return gulp.start(['babel', 'html', 'clean']);
+  return gulp.start(['babel', 'static']);
 });
